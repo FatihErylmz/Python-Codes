18ca30c86e51789d2fa5750ee5814d81694b2ce2
Fatih Eryılmaz <fatiheryilmaz432@gmail.com>
2024-09-11 16:51:38 +0300

    tuple

---
+++ tuple.py
@@ -0,0 +1,12 @@
+#TUPLE
+myList=[2,3,4]
+myTuple=(2,3,4) # ekleme güncelleme silme işlemleri yapılamaz değiştirilemez 
+
+myList[0]= 5 #hata vermez
+myTuple[0]= 6 #hata verir
+
+#listeden tuple dönüşüm
+myTuple2= tuple([3,8,9])
+
+#tuple liste dönüşümü
+myList2=list((1,2,3))
